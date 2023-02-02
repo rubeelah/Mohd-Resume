bd1249c83538ba0d5f053ae2f10e82e9ed9c8688
Rubeel Resume <malikbhai9026@gmail.com>
2023-02-03 04:17:26 +0530

    new logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,10 +1,11 @@
 import { Button, Container, Navbar, Nav } from 'react-bootstrap';
+import logo from "../assets/images/logo.png"
 
 function Header() {
   return (
     <Navbar bg="tranparent" expand="lg" className="main_header">
       <Container>
-        <Navbar.Brand href="#home" className='text_color_gradint logo'>Mohd Rubeel</Navbar.Brand>
+        <Navbar.Brand href="#home" className='text_color_gradint logo'><img src={logo} /> </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto w-100 justify-content-end main_navbar">
